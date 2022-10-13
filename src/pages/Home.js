9aae8667c2eb1cf414360ba7b5c4bacce4b3c31e
dylanboyer <boyer51@purdue.edu>
2022-10-13 09:21:15 -0400

    added a countdown timer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import { logout } from '../database/firebase'
+import CountDown from '../CountDown'
 
 class Home extends Component {
-
   signOut() {
     logout()
   }
-
+  
   render() {
     return (
+      <div>
       <div style={{textAlign: 'center'}}>
-        <h1>Welcome to Clockwork!</h1>
-        <button onClick = {this.signOut}>Logout</button>
-      </div>
-    )
+      <h1>Welcome to Clockwork!</h1>
+      <button onClick = {this.signOut}>Logout</button>
+      <button>Start</button>
+    </div>
+    <div>
+          <CountDown
+            onTimesup={() => {
+              alert(`Time's up!`)
+            }}
+            duration={65}
+          />
+        </div>
+    </div>
+
+    
+    );
   }
 }
 
