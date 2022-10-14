41ff58b35058008f2c342b8ff0ece4d405db4852
Manan Bhatia <bhatia47@purdue.edu>
2022-10-14 01:53:40 -0400

    fix updateTime, add .prettierrc

--- src/CountDown.js
+++ src/CountDown.js
@@ -1,59 +1,75 @@
 // https://github.com/haryphamdev/simple-react-countdown-timer/blob/master/src/CountDown.js
 
-
-import React from 'react';
-import Home from './pages/Home';
+import React from 'react'
+import Home from './pages/Home'
 
 export default class CountDown extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             count: props.duration ? props.duration : 5,
         }
     }
     componentDidMount() {
         this.timer = setInterval(() => {
-            let { count } = this.state;
+            let { count } = this.state
             if (count > 0) {
                 this.setState({
-                count: count - 1
-            }) }
+                    count: count - 1,
+                })
+            }
         }, 1000)
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         if (prevState.count !== this.state.count && this.state.count === 0) {
-            clearInterval(this.timer);
+            clearInterval(this.timer)
             if (this.props.onTimesup) {
-                this.props.onTimesup();
+                this.props.onTimesup()
             }
         }
     }
 
-    fmtMSS(s) { return (s - (s %= 60)) / 60 + (9 < s ? ':' : ':0') + s }
+    fmtMSS(s) {
+        return (s - (s %= 60)) / 60 + (9 < s ? ':' : ':0') + s
+    }
 
     updateTime() {
-        console.log("The function is being called")
+        console.log('The function is being called')
         const minutes = document.querySelector('#minutes').value
         const seconds = document.querySelector('#seconds').value
-    
+
         if (minutes <= 0 || seconds <= 0) {
-            alert("Please input a positive time.")
+            alert('Please input a positive time.')
         } else {
             this.setState({
-                count: minutes * 60 + seconds
+                count: minutes * 6 + seconds,
             })
         }
-    
-      }
+    }
 
     render() {
-        let { count } = this.state;
+        let { count } = this.state
         return (
             <div>
                 {this.fmtMSS(count)}
+                <div>
+                    <br></br>
+                    <div>How many minutes?</div>
+                    <input id="minutes" placeholder="Enter time in minutes" type="text"></input>
+                    <div>How many seconds?</div>
+                    <input id="seconds" placeholder="Enter time in seconds" type="text"></input>
+                    <div></div>
+                    <button
+                        style={{ margin: '10px' }}
+                        onClick={() => {
+                            this.updateTime()
+                        }}
+                    >
+                        Set Timer
+                    </button>
+                </div>
             </div>
         )
     }
 }
-
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,53 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { logout } from '../database/firebase'
 import CountDown from '../CountDown'
 
 class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            time: 1200,
+        }
+    }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: 1000
-    };
-} 
+    signOut() {
+        logout()
+    }
 
-  signOut() {
-    logout()
-  }
-
-  render() {
-    return (
-      <div>
-      <div style={{textAlign: 'center'}}>
-      <h1>Welcome to Clockwork!</h1>
-      <button onClick = {this.signOut}>Logout</button>
-      <button>Start</button>
-    </div>
-    <div>
-          <CountDown
-            onTimesup={() => {
-              alert(`Time's up!`)
-            }}
-            duration={this.state.time}
-          />
-          <div>
-            <br></br>
-            <div>How many minutes?</div>
-            <input id = "minutes" placeholder = "Enter time in minutes" type = "text"></input>
-            <div>How many seconds?</div>
-            <input id = "seconds" placeholder = "Enter time in seconds" type = "text"></input>
-            <div></div>
-            <button style={{margin: '10px'}} onClick={() => {
-                this.props.updateTime()
-            }}>Set Timer</button>
-          </div>
-        </div>
-    </div>
-
-
-    
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div style={{ textAlign: 'center' }}>
+                    <h1>Welcome to Clockwork!</h1>
+                    <button onClick={this.signOut}>Logout</button>
+                    <button>Start</button>
+                </div>
+                <div>
+                    <CountDown
+                        onTimesup={() => {
+                            alert(`Time's up!`)
+                        }}
+                        duration={this.state.time}
+                    />
+                    
+                </div>
+            </div>
+        )
+    }
 }
 
-export default Home;
+export default Home
